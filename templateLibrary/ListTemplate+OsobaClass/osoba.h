9b06c3205f355e8f621d009fad3c2dc297aea8c4
Kwinta <94843940+KwintaJ@users.noreply.github.com>
2022-05-17 15:47:09 +0200

    Update osoba.h

--- templateLibrary/ListTemplate+OsobaClass/osoba.h
+++ templateLibrary/ListTemplate+OsobaClass/osoba.h
@@ -9,32 +9,80 @@
 //############################################//
 
 #include <string>
+#include <iostream>
+#include <istream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
 
 #ifndef OSOBA
 #define OSOBA
 
 class Osoba
 {
-    public:
-        /* constructors */
-        explicit Osoba(std::string, int);
-        Osoba() = default;
+public:
+    /* constructors */
+    explicit Osoba(string i, int w): imie{i}, wiek{w}
+    {
+    }
+    Osoba() = default;
 
-        /* setters, getters */
-        void setImie(std::string);
-        void setWiek(int);
-        std::string getImie() const;
-        int getWiek() const;
-        
-        /* methods */
-        void edit();
-        std::ostream &print(std::ostream&) const;
-        std::istream &read(std::istream&);
-        bool operator<(const Osoba&);
+    /* setters, getters */
+    void setImie(string n)
+    {
+        imie=n;
+    } 
+    void setWiek(int a)
+    {
+        wiek=a;
+    }
+    string getImie() const
+    {
+        return imie;
+    }
+    int getWiek() const
+    {
+        return wiek;
+    }
+       
+    /* methods */
+    void edit()
+    {
+        cout << "Wprowadz nowe imie dla " << imie << ": ";
+        string noweImie;
+        getline(cin, noweImie);
+        if(!noweImie.empty())
+            this->imie = noweImie;
+        cout << "Wprowadz nowy wiek dla " << imie <<" (" << wiek << "): ";
+        string nowyWiek;
+        getline(cin, nowyWiek);
+        if(!nowyWiek.empty())
+            this->wiek = stoi(nowyWiek);
+    }
 
-    private:
-        std::string imie{""};
-        int wiek{0};
+    ostream &print(ostream& sout) const
+    {
+        sout << left << setw(12) << imie << right << setw(6) << wiek << endl;
+        return sout;
+    }
+
+    istream &read(istream& sin)
+    {
+        sin >> imie >> wiek;
+        return sin;
+    }
+
+    bool operator<(const Osoba &B)
+    {
+        if(imie == B.getImie())
+            return wiek < B.getWiek();
+        return imie < B.getImie();
+    }
+
+private:
+    string imie{""};
+    int wiek{0};
 };
 
 #endif
