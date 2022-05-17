b9617135366a1510ab833cdd396de1cf8d9447e7
Kwinta <94843940+KwintaJ@users.noreply.github.com>
2022-05-17 15:06:25 +0200

    Update main.cpp

--- templateLibrary/ListTemplate+OsobaClass/main.cpp
+++ templateLibrary/ListTemplate+OsobaClass/main.cpp
@@ -19,29 +19,6 @@ using namespace std;
 
 int main()
 {
-    /* List<int> liczby;
-    
-    int element;
-    while(cin >> element)
-    {
-        liczby.push_back(element);
-    }
-
-    liczby.sort();
-
-    cout << endl << "Mniejsze - wieksze:" << endl;
-    for(List<int>::iterator it = liczby.begin(); it != liczby.end(); ++it)
-    {
-        cout << *it << endl;
-    }
-
-    cout << endl << "Wieksze - mniejsze:" << endl;
-    for(List<int>::reverse_iterator it = liczby.reverse_begin(); it != liczby.reverse_end(); ++it)
-    {
-        cout << *it << endl;
-    }
-    */
-
     List<Osoba> grupa;
 
     ifstream fin;    
